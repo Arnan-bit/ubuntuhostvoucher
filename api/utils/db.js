256ab634e28acfde36f6f0a2be986dc773a8bd08
arnan <garudandne87@gmail.com>
2025-10-22 13:32:38 +0800

    Fix: Hapus opsi koneksi DB yang tidak valid

--- api/utils/db.js
+++ api/utils/db.js
@@ -11,9 +11,6 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    connectTimeout: 10000,
-    acquireTimeout: 10000,
-    timeout: 10000
 });
 
 export default db;
